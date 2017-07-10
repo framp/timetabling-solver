168322f6ecafb091f3239f5f08e7f9a092cb0916
Maurizio Lupo <maurizio.lupo@gmail.com>
2017-07-10 22:28:57 +0100

    test on building constraints

--- tests/minCollisions.test.js
+++ tests/minCollisions.test.js
@@ -47,3 +47,38 @@ tape('minCollisions, custom constraint', (assert) => {
     assert.true(fitness.pop.filter((individual) => individual.fitness === 0).length >= 1)
   })
 })
+
+tape('minCollisions, custom constraint composition', (assert) => {
+  assert.plan(2)
+
+  // this won't work because of function serialisation
+  // function getConstraint(time, item) {
+  //   return function (timetable) {
+  //     return timetable[time].indexOf(item) !== -1 ? [item] : []
+  //   };
+  // }
+
+  // this will work ;-)
+  function getConstraint(time, item) {
+    return new Function('timetable', `return timetable['${time}'].indexOf('${item}') !== -1 ? [item] : []`)
+  }
+
+  const slots = ['8:00', '10:00']
+  const bookables = ['Tennis', 'Climbing']
+  const constraints = [ // list of non overlapping constraints
+    ['Tennis', 'Climbing'],
+    getConstraint('8:00', 'Climbing')
+  ]
+
+  solve({ slots, bookables, constraints }, {
+    iterations: 10,
+    size: 3
+  }, (table, fitness) => {
+    const res = JSON.stringify(table)
+    assert.true(
+      res === '{"10:00":["Climbing"],"8:00":["Tennis"]}' ||
+      res === '{"8:00":["Tennis"],"10:00":["Climbing"]}'
+    )
+    assert.true(fitness.pop.filter((individual) => individual.fitness === 0).length >= 1)
+  })
+})
